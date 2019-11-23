4dc636b6c384875f91900903a2624437d04b2632
Paulo Menezes <paulo.hgmenezes@gmail.com>
2019-11-23 14:11:47 -0300

    fix port

--- src/index.js
+++ src/index.js
@@ -87,5 +87,5 @@ app.post('/api/persons', (request, response) => {
   response.status(201).json(newPerson);
 });
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
